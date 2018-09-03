82a14e087d158919af14f8130ba8765f61f0278b
Jere Valtteri Aho <jere.v.ahoreng.tamk.fi>
2018-09-03 17:18:51 +0300

    Viikkoharjoitus 2: Tehtava 3-4 toimii, taytyy lisata 3 ppl toiminto.

--- Viikkoharjoitus_2/src/com/company/Tehtava_3_4.java
+++ Viikkoharjoitus_2/src/com/company/Tehtava_3_4.java
@@ -1,4 +1,130 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
+class Person {
+    private String fname;
+    private String lname;
+    private String ssn; // social security number
+    private String phoneNum;
+    private Adress adr;
+
+    public Person(String firstName, String lastName, String socialSecurityNumber, String phoneNumber, Adress adress){
+
+
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public Adress getAdr() {
+        return adr;
+    }
+
+    public void setAdr(Adress adr) {
+        this.adr = adr;
+    }
+    @Override
+    public String toString(){
+        return "Person{" +
+                "first name=" + fname + '\'' +
+                ", last name=" + lname + '\'' +
+                ", Social Security number=" + ssn + '\'' +
+                ", phone number=" + phoneNum + '\'' +
+                ", adress=" + adr +
+                "}";
+    }
+
+}
+class Adress{
+    private String strAdress; // streetAdress
+    private String postNum;
+    private String myState;
+
+    public Adress (String streetAdress, String postNumber, String state){
+
+    }
+
+    public String getStreetAdress() {
+        return strAdress;
+    }
+
+    public void setStreetAdress(String streetAdress) {
+        this.strAdress = streetAdress;
+    }
+
+    public String getPostNum() {
+        return postNum;
+    }
+
+    public void setPostNum(String postNum) {
+        this.postNum = postNum;
+    }
+
+    public String getState() {
+        return myState;
+    }
+
+    public void setState(String myState) {
+        this.myState = myState;
+    }
+    @Override
+    public String toString(){
+        return "Adress{" +
+                ", street adress=" + strAdress + '\'' +
+                ", post number=" + postNum + '\'' +
+                ", state=" + myState + '\'' +
+                "}";
+    }
+
+}
+
+
+
 public class Tehtava_3_4 {
+    public static void main(String[] args){
+        Person[] ppl = new Person[1];
+        String fname = JOptionPane.showInputDialog("First name: ");
+        String lname = JOptionPane.showInputDialog("Last name: ");
+        String ssn = JOptionPane.showInputDialog("Social security number: ");
+        String phoneNum = JOptionPane.showInputDialog("Phone number: ");
+        String streetAdress = JOptionPane.showInputDialog("Sreet adress: ");
+        String postNumber = JOptionPane.showInputDialog("Post number: ");
+        String state = JOptionPane.showInputDialog("State: ");
+        ppl[0] = new Person(fname, lname, ssn, phoneNum, new Adress(streetAdress,postNumber,state));
+
+        for (Person h : ppl){
+            System.out.println(h);
+        }
+    }
 }
