b6c7eb241111cb6f9bee88ec607cd5e247baea20
Jere Valtteri Aho <jere.v.ahoreng.tamk.fi>
2018-09-17 15:43:52 +0300

    esimerkkiteht tehty

---
+++ Viikkoharjoitus_4/src/ThreadExample.java
@@ -0,0 +1,33 @@
+class Worker extends Thread {
+
+    private int sleepTime = 1000; // milliseconds
+    public Worker ( String name, int sleepTime){
+        super (name);
+        this.sleepTime = sleepTime;
+    }
+    public void run(){
+        for (int i = 0; i<20; i++){
+            System.out.println("Thread: " + getName() + " " + i);
+            try {
+                Thread.sleep( sleepTime );
+            }catch (InterruptedException ex){
+                System.out.println( ex );
+            }
+        }
+    }
+}
+
+public class ThreadExample{
+
+    public static void main(String[] args){
+        System.out.println("Luodaan saikeet...");
+        Worker worker1 = new Worker( "ykkössäie", 1500);
+        Worker worker2 = new Worker ("kakkosaie", 750);
+        Worker worker3 = new Worker ("kolmossaie", 2000);
+        worker1.start();
+        worker2.start();
+        worker3.start();
+    }
+
+
+}
---
+++ Viikkoharjoitus_4/src/saikeet.java
@@ -0,0 +1,8 @@
+
+
+
+public class saikeet {
+
+private int sleepTime = 1000; // milliseconds
+
+}
